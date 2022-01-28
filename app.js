989c162f9abf042da55ee764669939f79d2dec99
divent1 <fmdiventone@gmail.com>
2022-01-28 15:33:58 +0100

    3

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = '#0a0a0a';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
